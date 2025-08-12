e37d2f8cc95e63ec27b5c7fa2b699dde5f1204e2
Shashank-9676 <shashankchowdary09@gmail.com>
2025-08-12 12:13:59 +0530

    Updating cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
 dotenv.config()
 app.use(express.json())
+app.use(cors())
 mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser : true
 })
